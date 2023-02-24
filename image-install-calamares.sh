000b37411e4d42339dccce90481541a106d39268
endeavouros-arm <67989319+endeavouros-arm@users.noreply.github.com>
2023-02-23 19:26:32 -0600

    Update image-install-calamares.sh

--- image-install-calamares.sh
+++ image-install-calamares.sh
@@ -39,7 +39,7 @@ _choose_filesystem_type() {
          ext4) FILESYSTEMTYPE="ext4" ;; 
          btrfs) FILESYSTEMTYPE="btrfs" ;;
      esac
-     printf "\n\nFilesystem type = FILESYSTEMTYPE\n\n"
+     printf "\n\nFilesystem type = $FILESYSTEMTYPE\n\n"
      read z
 }
 
